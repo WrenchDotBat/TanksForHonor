b787370d377db042918c59eff53c2d2f06465364
FunMaster <wrenchdotbat@gmail.com>
2018-03-30 10:51:10 +0600

    Init commit and landscape testing

---
+++ Source/TanksForHonor/TanksForHonorGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TanksForHonorGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKSFORHONOR_API ATanksForHonorGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
